05ba8c98a4590a11637e9a68c67543685c390218
obscurehistoricalfigure <obscurehistoricalfigure@protonmail.com>
2020-07-29 23:49:34 -0400

    updated stub code for autoban

--- map/signals.py
+++ map/signals.py
@@ -5,8 +5,9 @@ from django_eventstream import send_event
 from rest_framework.renderers import JSONRenderer
 from rest_framework.authtoken.models import Token
 
-from map.models import Event
+from map.models import Event, Report
 from map.serializers import EventSerializer
+
 
 @receiver(post_save, sender=Event)
 def send_update(sender, instance, created=False, **kwargs):
@@ -15,7 +16,24 @@ def send_update(sender, instance, created=False, **kwargs):
             serializer = EventSerializer(instance)
             send_event(instance.city, 'message', serializer.data)
 
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
+
+
+# # UNTESTED, we want to see how the reports look before automating the banning process
+# @receiver(post_save, sender=Report)
+# def process_report(sender, instance, created=False, **kwargs):
+#     if created:
+#         reported_user = instance.event.user
+#         event_reports = Report.objects.filter(event=instance.event).count()
+#         total_reports = Report.objects.filter(event__user=reported_user).count()
+#         # this doesn't deal with one person reporting the same events multiple times
+#         # maybe count unique users amounst the reports? or unique per report?
+#         if total_reports >= 100:
+#             # regenerate the users token key to lock them out
+#             token = Token.objects.get(user=reported_user)
+#             token.key = token.generate_key()
+#             token.save()
--- mapbackend/settings.py
+++ mapbackend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 VERSION = '0.1.6'
 
 # TBD: FIX, DEVELOPMENT ONLY
